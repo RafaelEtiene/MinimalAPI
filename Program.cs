33dcc5a80715877e70a177eedfd36f8c14898441
Rafael Etiene <rafaellimaetiene@gmail.com>
2023-06-05 12:29:34 -0300

    Criação dos métodos HTTP

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Data;
+using MinimalAPI.Models;
+using MiniValidation;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<MinimalContextDb>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,28 +23,74 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/fornecedor", async (
+    MinimalContextDb context) =>
+    await context.Fornecedores.ToListAsync())
+.WithName("GetFornecedor")
+.WithTags("Fornecedor");
 
-app.MapGet("/weatherforecast", () =>
+
+app.MapGet("/fornecedor/{id}", async (
+    Guid id,
+    MinimalContextDb context) =>
+    await context.Fornecedores.FindAsync(id)
+    is Fornecedor fornecedor ? Results.Ok(fornecedor) : Results.NotFound()
+)
+.Produces<Fornecedor>(StatusCodes.Status200OK)
+.Produces<Fornecedor>(StatusCodes.Status404NotFound)
+.WithName("GetFornecedorPorId")
+.WithTags("Fornecedor");
+
+
+app.MapPost("fornecedor", async (MinimalContextDb context, Fornecedor fornecedor) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+
+
+    context.Fornecedores.Add(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0 ? Results.Created($"/fornecedor/{fornecedor.Id}", fornecedor) : Results.BadRequest("Houve um erro ao salvar o registro");
 })
-.WithName("GetWeatherForecast");
+.Produces<Fornecedor>(StatusCodes.Status201Created)
+.Produces<Fornecedor>(StatusCodes.Status400BadRequest)
+.WithName("PostFornecedor")
+.WithTags("Fornecedor");
+
+
+app.MapPut("/fornecedor/{id}", async (Guid id, Fornecedor fornecedor, MinimalContextDb context) =>
+{
+    var fornecedorBanco = await context.Fornecedores.FindAsync(id);
+    if (fornecedorBanco == null) return Results.NotFound();
+
+    if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+
+    context.Fornecedores.Update(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0 ? Results.NoContent() : Results.BadRequest("Houver um erro ao salvar o registro");
+})
+.Produces<Fornecedor>(StatusCodes.Status204NoContent)
+.Produces<Fornecedor>(StatusCodes.Status400BadRequest)
+.WithName("PutFornecedor")
+.WithTags("Fornecedor");
+
+
+app.MapDelete("fornecedor/{id}", async (MinimalContextDb context, Guid id) =>
+{
+    var fornecedor = await context.Fornecedores.FindAsync(id);
+    if (fornecedor == null) return Results.NotFound();
+
+    context.Fornecedores.Remove(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0 ? Results.NoContent() : Results.BadRequest("Houve um erro ao deletar o registro");
+})
+.Produces<Fornecedor>(StatusCodes.Status204NoContent)
+.Produces<Fornecedor>(StatusCodes.Status400BadRequest)
+.WithName("DeleteFornecedor")
+.WithTags("Fornecedor");
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
